73f8df8b237578cb0236e1cd8b89db8fb8d948aa
BradleyTim <timbradley94@gmail.com>
2019-04-16 07:35:51 +0300

    add serviceWorker, images and favicons

--- sw.js
+++ sw.js
@@ -1,17 +1,47 @@
-let version = 1;
-let cacheName = `cache-v${version++}`;
-const cacheFiles  = [
-  './',
-  './index.html',
-  './offline.html',
-  './css/style.css',
-  './css/offline.css',
-  './js/app.js',
-  './images/icons/icon-192x192.png',
-  './images/icons/icon-384x384.png',
-  './images/icons/icon-512x512.png',
-  './manifest.json',
-  'https://fonts.googleapis.com/css?family=Roboto'
+let version = 2;
+let cacheName = `cache-v${version}`;
+const cacheAssetFiles  = [
+  '/',
+  '/index.html',
+  '/offline.html',
+  '/css/style.css',
+  '/css/offline.css',
+  '/js/app.js',
+  '/images/icons/icon-72x72.png',
+  '/images/icons/icon-96x96.png',
+  '/images/icons/icon-128x128.png',
+  '/images/icons/icon-144x144.png',
+  '/images/icons/icon-152x152.png',
+  '/images/icons/icon-192x192.png',
+  '/images/icons/icon-384x384.png',
+  '/images/icons/icon-512x512.png',
+  '/favs/android-icon-36x36.png',
+  '/favs/android-icon-48x48.png',
+  '/favs/android-icon-72x72.png',
+  '/favs/android-icon-96x96.png',
+  '/favs/android-icon-144x144.png',
+  '/favs/android-icon-192x192.png',
+  '/favs/apple-icon-57x57.png',
+  '/favs/apple-icon-60x60.png',
+  '/favs/apple-icon-72x72.png',
+  '/favs/apple-icon-76x76.png',
+  '/favs/apple-icon-114x114.png',
+  '/favs/apple-icon-120x120.png',
+  '/favs/apple-icon-144x144.png',
+  '/favs/apple-icon-152x152.png',
+  '/favs/apple-icon-180x180.png',
+  '/favs/apple-icon-precomposed.png',
+  '/favs/apple-icon.png',
+  '/favs/browserconfig.xml',
+  '/favs/favicon-16x16.png',
+  '/favs/favicon-32x32.png',
+  '/favs/favicon-96x96.png',
+  '/favs/favicon.ico',
+  '/favs/ms-icon-70x70.png',
+  '/favs/ms-icon-144x144.png',
+  '/favs/ms-icon-150x150.png',
+  '/favs/ms-icon-310x310.png',
+  '/manifest.json'
 ];
 
 self.addEventListener('install', (e) => {
@@ -22,7 +52,7 @@ self.addEventListener('install', (e) => {
     caches.open(cacheName)
     .then(cache => {
       console.log('caching files...');
-      return cache.addAll(cacheFiles);
+      return cache.addAll(cacheAssetFiles);
     })
   );
 });
