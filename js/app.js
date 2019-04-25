a8ef7ebaf17832db8f8542412a79bf153aecd41c
BradleyTim <timbradley94@gmail.com>
2019-04-25 11:42:15 +0300

    code refactor

--- js/app.js
+++ js/app.js
@@ -2,28 +2,27 @@
 
 // FETCH THE WEATHER DATA FROM API
 const fetchWeather = async () => {
-  const city = document.querySelector('#city').value.charAt(0).toUpperCase();
-  const country = document.querySelector('#country').value.charAt(0).toUpperCase();
+  const city = document.querySelector("#city").value.toLowerCase();
+  const country = document.querySelector("#country").value.toLowerCase();
 
   try {
-    const API_KEY = 'cf701ba1fa0b6880bd6c1a3d23e41be5';
-    const proxy = 'https://cors-anywhere.herokuapp.com/';
-    const api_call = await fetch(`${proxy}https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+    const API_KEY = "cf701ba1fa0b6880bd6c1a3d23e41be5";
+    const proxy = "https://cors-anywhere.herokuapp.com/";
+    const api_call = await fetch(
+      `${proxy}https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`
+    );
 
     const response = await api_call.json();
 
     displayResults(response);
-
   } catch (error) {
     console.log(error);
   }
-
-}
+};
 
 // DISPLAY THE WEATHER RESPONSE
 const displayResults = response => {
-
-  const container = document.querySelector('.container');
+  const container = document.querySelector(".container");
 
   if (response.main) {
     container.innerHTML = `
@@ -42,26 +41,26 @@ const displayResults = response => {
   } else {
     container.innerHTML = `<div class="error">Enter a valid city and country</div>`;
   }
-}
+};
 
 // REGISTER THE SERVICEWORKER FOR PWA CAPABILITIES
-window.addEventListener('load', () => {
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./sw.js')
+window.addEventListener("load", () => {
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+      .register("./sw.js")
       .then(registration => {
-        console.log('{serviceWorker} registered');
+        console.log("{serviceWorker} registered");
       })
       .catch(err => {
-        console.log('There was an error');
+        console.log("There was an error");
       });
   }
 });
 
 // CALL FETCHWEATHER FUNCTION WHEN BUTTON IS CLICKED
-const button = document.querySelector('#search');
-button.addEventListener('click', (e) => {
+const button = document.querySelector("#search");
+button.addEventListener("click", e => {
   e.preventDefault();
 
   fetchWeather();
-
 });
