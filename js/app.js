ecf06fcd69cf50a548c5e6f70f7a94935e9fbd49
BradleyTim <timbradley94@gmail.com>
2019-12-01 16:58:12 +0300

    refactor dynamic html templates

--- js/app.js
+++ js/app.js
@@ -20,7 +20,7 @@ function getWeather() {
           return res.json();
         })
         .then(data => {
-          //console.log(data);
+          // console.log(data);
           displayResults(data);
         })
         .catch(error => console.log("something went wrong", error));
@@ -33,23 +33,64 @@ function getWeather() {
 // DISPLAY THE WEATHER RESPONSE
 function displayResults(data) {
   const container = document.querySelector(".container");
+  container.textContent = 'Loading...'
 
   if (data.timezone) {
-    container.innerHTML = `
-    <div><span>Timezone:</span> ${data.timezone}</div>
-    <div><span>Temperature:</span> ${((data.currently.temperature - 32) *
-      (5 / 9)).toFixed(1)}°c</div>
-    <div><span>Humidity:</span> ${(data.currently.humidity * 100).toFixed(0)}%</div>
-    <div><span>${data.currently.summary} </div>
-    `;
+
+    const timezoneDiv = document.createElement('div');
+    timezoneDiv.textContent = `Timezone, ${data.timezone}`;
+
+    const summaryDiv = document.createElement('div');
+    summaryDiv.textContent = `${data.currently.summary}`;
+
+    const tempDiv = document.createElement('div');
+    tempDiv.textContent = `Temperature, ${((data.currently.temperature - 32) * (5 / 9)).toFixed(1)}°c`;
+
+    const humidityDiv = document.createElement('div');
+    humidityDiv.textContent = `Humidity, ${(data.currently.humidity * 100).toFixed(0)}%`;
+
+    container.textContent = '';
+
+    container.appendChild(timezoneDiv);
+    container.appendChild(summaryDiv);
+    container.appendChild(tempDiv);
+    container.appendChild(humidityDiv);
+    // container.innerHTML = `
+    // <div><span>Timezone:</span> ${data.timezone}</div>
+    // <div><span>Temperature:</span> ${((data.currently.temperature - 32) *
+    //   (5 / 9)).toFixed(1)}°c</div>
+    // <div><span>Humidity:</span> ${(data.currently.humidity * 100).toFixed(0)}%</div>
+    // <div><span>${data.currently.summary} </div>
+    // `;
   } else if (data.offline) {
-    container.innerHTML = `
-    <div class="offline">
-      <h1>${data.offline.title}</h1>
-      <p>${data.offline.msg}</p>
-    </div>`;
+    const div = document.createElement('div');
+    dispatchEvent.classList.add('offline');
+
+    const h1 = document.createElement("h1");
+    h1.textContent = data.offline.title;
+
+    const p = document.createElement("p");
+    p.textContent = data.offline.msg;
+
+    div.appendChild(h1);
+    div.appendChild(p);
+
+    container.textContent = "";
+
+    container.appendChild(div);
+
+
+    // container.innerHTML = `
+    // <div class="offline">
+    //   <h1>${data.offline.title}</h1>
+    //   <p>${data.offline.msg}</p>
+    // </div>`;
   } else {
-    container.innerHTML = `<div class="error">Kindly turn on your location</div>`;
+    const div = document.createElement('div');
+    div.classList.add('error');
+    container.textContent = "";
+    container.appendChild(div);
+    // container.innerHTML = `<div class="error">Kindly turn on your location</div>`;
   }
 }
 
